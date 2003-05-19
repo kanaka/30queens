c3011d7482fe24b07312fe4b47b767fb8af8d949
Joel Martin <jmartin@sentryds.com>
2003-05-19 12:52:00 -0400

    h.c 625 bytes: the double compile hack.
    Compile once to determine the location of collseqmb which is a array
    from 0 -> 256. But it's not normally referenceable.
    
    Compile a second time with that address.

--- Makefile
+++ Makefile
@@ -9,8 +9,8 @@ min: min.c
 min2: min2.c
 	$(CC) $(CFLAGS) $^ -o $@
 
-hack: hack.c
-	$(CC) $(CFLAGS) $^ -o $@
+h: h.c
+	a="g++ -include /usr/include/stdlib.h -include /usr/include/stdio.h -O3 -static -Wl,--omagic h.c -oh -DU=unsigned -DC=char -DO=const -DY=main -DZ=for -DD=0x0"; $$a; $$a`nm h|grep qm|sed 's/0 .*$$/1/'`
 
 clean:
 	rm full
--- h.c
+++ h.c
@@ -1,18 +1,9 @@
-/* gcc -O3 -static hack.c -o hack */
-unsigned char a,b=-1,i,j,x,z,l[2015],*m=l+30;
-int cnt;
-main(y){
-  int tries=0;
-  //bcopy(0x80a7561,l,30);
-  for (i=0; i<30; i++) {
-  //    printf ("%d\n", l[i]);
-    l[i]=i+1;
-  }
-  printf("%lx\n", &main);
-  for(;b;b<a?z=l[y],l[y]=l[x],l[x]=z:(b=a),x=rand(),y=x%29,x%=30,tries++)
-    for(a=i=0,j=1,z=l[y],l[y]=l[x],l[x]=z;j>29?j=++i+1:1,i<29;j++)
-      (l[i]==(l[j]+i-j))||(l[i]==(l[j]-i+j))?a++:1,j%2&&a>100?a--:1;
-  for(i=j=0;(j>30)?i++,j=0:1,i<32;j++,j==30&&i%31?m[i*62+l[i-1]*2-1]='Q':0)
-    m[i*62+j*2]=i%31?'|':'+',m[i*62+j*2+1]=j==30?'\n':'-',i*j==930?puts(m):1;
-  printf("tries: %d\n", tries);
+/* a="g++ -include /usr/include/stdlib.h -include /usr/include/stdio.h -O3 -static -Wl,--omagic h.c -oh -DU=unsigned -DC=char -DO=const -DY=main -DZ=for -DD=0x0"; $a; $a`nm h|grep qm|sed 's/0 .*$/1/'` */
+U C J,M,_,A,B=-1,F=30,G=31,H=62,m[2003],x,*l=(U C*)D,*c=(U C*)D,*d=(U C*)D+1;
+Y(){
+  Z(;B;B<A?M=*d,*d=*c,*c=M:(B=A),x=rand(),d=l+x%29,c=l+x%F)
+    Z(A=_=0,J=1,M=*d,*d=*c,*c=M;J>29?J=++_+1:1,_<29;J++)
+      (l[_]==(l[J]+_-J))||(l[_]==(l[J]-_+J))?A++:1,J%2&&A>100?A--:1;
+  Z(_=J=0;(J>F)?_++,J=0:1,_<=G;J++,J==F&&_%G?m[_*H+l[_-1]*2-1]='Q':0)
+    m[_*H+J*2]=_%G?'|':'+',m[_*H+J*2+1]=J==F?'\n':'-',_*J==F*G?puts((O C*)m):1;
 }
