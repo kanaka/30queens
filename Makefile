8bd8f906cc6837d9b4cb35fddf1614b1cc623f5a
Joel Martin <jmartin@sentryds.com>
2003-11-05 11:45:00 -0400

    h.c 488: remove spaces from command line.

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ min2: min2.c
 	$(CC) $(CFLAGS) $^ -o $@
 
 h: h.c
-	a="g++ -fpermissive -includestdlib.h -includestdio.h -O3 -static -Wl,--omagic h.c -oh -oh -DS=M=*d,*d=*c,*c=M -DD=0x0";$$a;$$a`nm h|grep qm|sed 's/0 .*$$/1/'`
+	a="g++ -fpermissive -includestdlib.h -includestdio.h -O3 -static -Wl,--omagic h.c -oh -DS=M=*d,*d=*c,*c=M -DD=0x0";$$a;$$a`nm h|grep qm|sed 's/0 .*$$/1/'`
 
 clean:
 	rm full
--- h.c
+++ h.c
@@ -1,5 +1,5 @@
-/* a="g++ -fpermissive -includestdlib.h -includestdio.h -O3 -static -Wl,--omagic
-h.c -oh -DS=M=*d,*d=*c,*c=M -DD=0x0";$a;$a`nm h|grep qm|sed 's/0 .*$/1/'` */
+/*a="g++ -fpermissive -includestdlib.h -includestdio.h -O3 -static -Wl,--omagic
+h.c -oh -DS=M=*d,*d=*c,*c=M -DD=0x0";$a;$a`nm h|grep qm|sed 's/0 .*$/1/'`*/
 unsigned char J,M,_,A,B=255,E=29,F=30,G=31,m[2003],*n=m,*l=D,*c=D,*d=D+1;
 main(){
  for(;A=_=J=0,B;B<A?S:B=A,d=l+(M=rand())%E,c=l+M%F)
